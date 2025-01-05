a21087f965823a01ed684d494d316bcb840e76c4
eapepe0 <eapepe0@gmail.com>
2025-01-04 23:30:28 -0300

    - Vamos a permitir que se muestren imágenes en nuestro feed y que se vea un poco mas lindo.
    - Instalaremos las dependencias :
       npm i sanitize-html markdown-it
       npm i -D @types/markdown-it @types/sanitize-html
    
    - Modificamos el archivos rss.xml.ts
      Parseamos el Markdown y permitimos que se muestren imágenes.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,6 +1,10 @@
 import type { APIRoute } from "astro";
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+
+const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ params, request , site }) => {
     // en los params vienen los parámetros de la URL
@@ -20,16 +24,30 @@ export const GET: APIRoute = async ({ params, request , site }) => {
         // Pull in your project "site" from the endpoint context
         // https://docs.astro.build/en/reference/api-reference/#site
         site: site ?? '',
+        xmlns: {
+            media: 'http://search.yahoo.com/mrss/',
+          },
         // Array of `<item>`s in output xml
         // See "Generating items" section for examples using content collections and glob imports
 
         // mapeamos los blog de blogPost
         // extraemos data y slug de cada blog y devolvemos un objeto con los datos necesarios
-        items: blogPost.map(({data , slug}) => ({
+        items: blogPost.map(({data , slug , body}) => ({
             title : data.title,
             pubDate : data.date,
             description : data.description,
             link : `/posts/${slug}`,
+            content: sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+              }),
+              
+              customData: `<media:content
+                  type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+                  width="${data.image.width}"
+                  height="${data.image.height}"
+                  medium="image"
+                  url="${site + data.image.src}" />
+              `,
         })),
         // (optional) inject custom xml
         customData: `<language>en-us</language>`,
